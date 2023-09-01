c5bdbf0366738016930310a7987971dd081acd96
koray demir <koraydemirmc@gmail.com>
2023-09-01 15:37:20 +0300

    html düzenlemeleri - Input bug düzenlemesi - SignUp sistemi Tamamlandı - Bug düzenlemeleri - BackGround JS eklentisi

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,43 +9,44 @@ const panelContainer = document.querySelector(".panelContainer")
 const backImage = document.querySelector(".backImage")
 const form = document.querySelector(".form")
 const loginAllDiv = document.querySelector(".login-all")
-
-let doOnce = 0;
+const blurryImage = document.querySelector(".blurry-image")
 
 signUpBtn.addEventListener("click",(e) => {
     e.preventDefault()
-
-
-    loginAllDiv.innerHTML = `<p class="text">SignUp</p>
-    <div class="Username">
-        <input class="input-name" placeholder="Username.." type="name">
-    </div>
-    <div class="password">
-        <input class="input-phone active" placeholder="Phone.." type="text">
-        <input class="input-password" placeholder="Generate Password.." type="text">
-    </div>
-    <div class="buttons">
-        <button class="login-btn">Login</button>
-        <button class="signup-btn">Sign Up</button>
-    </div>
-    <button class="forgot-passwordBtn">Forgot Password</button>`
-
+    if(phoneInput.className === "input-phone active"){
+        signUp()
+    }
+    nameInput.classList.remove("warn");
+    passwordInput.classList.remove("warn");
+    blurryImage.classList.add("active")
+    phoneInput.classList.add("active")
+    
+    
 });
+
+
 
 logInBtn.addEventListener("click",(e) => {
     e.preventDefault()
-    
-    if(nameInput.value === ""){
-        nameInput.classList.add("warn");
-        return;
-    } else if (passwordInput.value === ""){
-        nameInput.classList.remove("warn");
-        passwordInput.classList.add("warn");
-        return;
-    } else {
-        passwordInput.classList.remove("warn");
-        logIn();
-        return;
+    nameInput.classList.remove("warn");
+    passwordInput.classList.remove("warn");
+
+    if(phoneInput.className === "input-phone active"){
+        phoneInput.classList.remove("active")
+        return
+    }else {
+        if(nameInput.value === ""){
+            nameInput.classList.add("warn");
+            return;
+        } else if (passwordInput.value === ""){
+            nameInput.classList.remove("warn");
+            passwordInput.classList.add("warn");
+            return;
+        } else {
+            passwordInput.classList.remove("warn");
+            logIn();
+            return;
+        }
     }
     
     e.preventDefault()
@@ -76,35 +77,36 @@ const fetchData = async (url) => {
 }
 
 const postData = async (url, data) => {
-    return await fetch(url, {
+    // ChatGpt eseri
+    await fetch(url, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
-            'Authorization': 'Bearer your_access_token_here',
-            'apikey': 'your_api_key_here'
+            'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd4d3BndGZyenR2ZXFncXlja25xIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5MzQxMjM4NCwiZXhwIjoyMDA4OTg4Mzg0fQ.fxxdxsJkoR5d_1IsCiar6iiGa2WUi5UWAPo_N_dXggg',
+            'apikey': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd4d3BndGZyenR2ZXFncXlja25xIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5MzQxMjM4NCwiZXhwIjoyMDA4OTg4Mzg0fQ.fxxdxsJkoR5d_1IsCiar6iiGa2WUi5UWAPo_N_dXggg'
         },
-        body: JSON.stringify(data)  // Göndermek istediğiniz veriyi JSON formatına dönüştürüp ekleyin
+        body: JSON.stringify(data) 
     }).then(response => response.json());
+    window.location.reload()
 }
 
 async function signUp () {
+    
     let data = {
         userName: `${nameInput.value}`,
         phone: `${phoneInput.value}`,
         password: `${passwordInput.value}`
-
     }
-    postData('https://example.com/api', data)
-    .then(responseData => {
-        console.log('POST Response:', responseData);
-    })
-    .catch(error => {
-        console.error('POST Error:', error);
-    });
+    postData('https://gxwpgtfrztveqgqycknq.supabase.co/rest/v1/users', data);
+    
+
+    
 }
 
 
 async function logIn () {
+    nameInput.classList.remove("warn");
+    passwordInput.classList.remove("warn");
     let users = await fetchData("https://gxwpgtfrztveqgqycknq.supabase.co/rest/v1/users")
     if(users.find(x =>  nameInput.value === x.userName&&users.find(x =>  passwordInput.value === x.password))){
         panelContainer.innerHTML = ``
